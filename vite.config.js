ac332656498ab7ba3686f8001715d3deb707d43e
jomichaelis <johannesmichaelis0@gmail.com>
2023-09-24 22:57:46 +0200

    added vite-pwa configuration and assets

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
+// PWA
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -18,6 +21,31 @@ export default defineConfig({
       styles: {
         configFile: 'src/styles/settings.scss',
       },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['logo_rot_filled.ico', 'logo_rot_filled.svg', 'logo_rot_filled.png'],
+      manifest: {
+        name: 'Silberfuchs Management System',
+        short_name: 'SMS',
+        description: 'Alles an einem Ort',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo_rot_filled_192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo_rot_filled_512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
   ],
   define: { 'process.env': {} },
